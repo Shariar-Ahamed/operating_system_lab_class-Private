e91f17d8f15946c23ae0b3a10c005c512ee1c154
Shariar Ahamed <shahriar64.diu@gmail.com>
2025-10-08 09:25:30 +0600

    Update and rename Thard_Lab_Class.sh to Second_Lab_Class.sh

---
+++ 2nd Lab Class/Second_Lab_Class.sh
@@ -0,0 +1,50 @@
+echo "Hello World";
+
+#Comment "This is comment"
+<<Com
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+Com
+
+#Variable
+
+num1=100
+num2=15
+sum=$((num1/num2))
+
+echo $num1 and $num2 = $sum
+
+
+
+#User input
+
+read -p "Enter a 1st number = " num1 
+read -p "Enter a 2nd number = " num2
+read -p "Enter a 3rd number = " num3
+
+sum=$((num1+num2+num3))
+
+echo -n "Sum = $sum"
+
+avg=$((sum/3))
+
+echo " Avg = $avg"
+
+
+
+#Conditional Operator
+
+num1=10
+num2=20
+
+if (($num1>$num2))
+then
+    echo "Greater"
+elif [ $num1 -lt $num2 ]
+then
+    echo "Less"
+else
+    echo "Equal"
+fi
