fabe63f579d3aba0260415339cde3ae185acc655
Shariar Ahamed <shahriar64.diu@gmail.com>
2025-10-08 09:53:46 +0600

    Create problem-2.sh

---
+++ 2nd Lab Class/problem-2.sh
@@ -0,0 +1,26 @@
+#problem-2 Single
+
+read a b c
+
+if ((a>=b))
+then 
+    if ((a>=c))
+    then
+        echo "A"
+    else
+        echo "C"
+    fi
+else 
+    if ((b>=C))
+    then 
+        echo "B"
+    else 
+        echo "C"
+    fi
+fi 
+
+<<com
+Output:
+10 2 12
+C
+com
